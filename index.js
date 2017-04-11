34a5a64617be41a463e444f9c82a29204c74e1b5
Jake Crampton <jake.crampton@ossm.edu>
2017-04-11 16:13:18 -0500

    Made the app get voice options from the database

--- index.js
+++ index.js
@@ -2,29 +2,65 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var exec = require('child_process').exec;
 
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'sayapp'
+});
+
+connection.connect();
+
 var app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.get('/users', function(req, res) {
+    var selectStatement = 'SELECT * FROM users';
+    var options = [];
+    connection.query(selectStatement, function(error, results, fields) {
+        for (var i = 0; i < results.length; i++) {
+            var optionString = '<option value=\"' + results[i].user_id + '\">' + results[i].name + '</option>';
+            options.push(optionString);
+        }
+        res.send(options);
+    })
+
+})
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(express.static('public'));
 
-app.post('/', function (req, res) {
-   console.log("Got a POST request for the homepage");
-	 console.log(req.body);
-	 var voice="\""+req.body.voice.replace(/\W/g,"")+"\"";
-	 var text="\""+req.body.text.replace(/\W/g,"")+"\"";
-	 console.log(text);
+app.post('/', function(req, res) {
+    console.log("Got a POST request for the homepage");
+    console.log(req.body);
+    var voice = "\"" + req.body.voice.replace(/\W/g, "") + "\"";
+    var text = "\"" + req.body.text.replace(/\W/g, "") + "\"";
+    console.log(text);
+    var selectStatement = 'SELECT * FROM users WHERE user_id=' + voice + ';';
 
-	 var cmd = 'say -v '+voice+' '+text;
+    connection.query(selectStatement, function(error, results, fields) {
+        var voiceName = results[0].name;
+        var insertStatement = 'INSERT INTO messages (body, owner) VALUES (' + text + ',' + results[0].user_id + ')';
 
-	 exec(cmd, function(error, stdout, stderr) {
-  	// command output is in stdout
-		});
+        connection.query(insertStatement, function(error, results, fields) {
+            var cmd = 'say -v ' + voiceName + ' ' + text;
 
-   res.send('Hello POST');
+            exec(cmd, function(error, stdout, stderr) {
+                // command output is in stdout
+            });
+            res.send('Hello POST');
+
+            if (error) throw error;
+            console.log('success');
+        });
+    });
 })
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
 
-   console.log("Example app listening at http://%s:%s", host, port)
+var server = app.listen(8081, function() {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Example app listening at http://%s:%s", host, port)
 })
--- public/scripts/javascript.js
+++ public/scripts/javascript.js
@@ -1,13 +1,28 @@
 var text;
 var option;
-$(document).ready(function(){
-	$("#form").submit(function(event){
-		event.preventDefault();
-		text=$("#textbox").val();
-		option=$("#drop").val();
-		$.post( "/", { voice: option, text: text } );
+$(document).ready(function() {
 
-		console.log(text);
-		console.log(option);
-	});
+    $.get('/users', {}, function success(data) {
+        console.log(data);
+        for (var i = 0; i < data.length; i++) {
+            $("#drop").append(data[i]);
+        }
+    });
+
+
+
+    $("#form").submit(function(event) {
+        event.preventDefault();
+        text = $("#textbox").val();
+        option = $("#drop").val();
+        $.post("/", {
+            voice: option,
+            text: text
+        }, function success(data) {
+            console.log(data);
+        });
+
+        console.log(text);
+        console.log(option);
+    });
 })
