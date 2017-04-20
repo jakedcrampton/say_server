37e862138082de2e23f1f06df51d62fdd30fa597
Jake Crampton <jake.crampton@ossm.edu>
2017-04-20 15:33:15 -0500

    Switched from being mysql dependent to using the sqlize orm

--- index.js
+++ index.js
@@ -2,54 +2,80 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var exec = require('child_process').exec;
 
-var mysql = require('mysql');
-var connection = mysql.createConnection({
+var SQ = require('sequelize');
+
+var sequelize = new SQ('sayapp', 'root', '', {
     host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'sayapp'
+    dialect: 'mysql',
+
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    },
 });
 
-connection.connect();
+var User = sequelize.define('users', {
+    name: {
+        type: SQ.STRING,
+        field: 'name' // Will result in an attribute that is firstName when user facing but first_name in the database
+    },
+    uid: {
+        type: SQ.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        field: 'user_id'
+    }
+}, {
+    timestamps: false
+});
 
 var app = express();
 
 app.get('/users', function(req, res) {
     var selectStatement = 'SELECT * FROM users';
-    var options = [];
-    connection.query(selectStatement, function(error, results, fields) {
+    User.findAll().then(function(results) {
         res.send(results);
     })
-
 })
 
 app.use(bodyParser.urlencoded({
     extended: false
 }));
+
 app.use(express.static('public'));
 
 app.post('/', function(req, res) {
+
     console.log("Got a POST request for the homepage");
     console.log(req.body);
-    var voice = "\"" + req.body.voice.replace(/\W/g, "") + "\"";
+
+    var voice = req.body.voice.replace(/\W/g, ""); //It wasnt recognizing the /" as not existing, so idk man. Probably because that's a string and not a SQ.INTEGER
     var text = "\"" + req.body.text.replace(/\W/g, "") + "\"";
+
     console.log(text);
-    var selectStatement = 'SELECT * FROM users WHERE user_id=' + voice + ';';
 
-    connection.query(selectStatement, function(error, results, fields) {
+    User.findAll({
+        where: {
+            uid: voice
+        }
+    }).then(function(results) {
         var voiceName = results[0].name;
-        var insertStatement = 'INSERT INTO messages (body, owner) VALUES (' + text + ',' + results[0].user_id + ')';
+        //var insertStatement = 'INSERT INTO messages (body, owner) VALUES (' + text + ',' + results[0].uid + ')';
 
-        connection.query(insertStatement, function(error, results, fields) {
+        User.create({
+            body: text,
+            owner: results[0].uid
+        }).then(function(results) {
             var cmd = 'say -v ' + voiceName + ' ' + text;
 
             exec(cmd, function(error, stdout, stderr) {
                 // command output is in stdout
+                res.send('Hello POST');
             });
-            res.send('Hello POST');
 
-            if (error) throw error;
-            console.log('success');
+            /*if (error) throw error;
+            console.log('success');*/
         });
     });
 })
